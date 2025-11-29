a06bb88da63970d1231f5953619771a5b6614772
Alexander Topalo <topaloalexander@gmail.com>
2025-11-29 16:03:14 +0200

    add throws

--- src/utils/validateDataUsingZod.ts
+++ src/utils/validateDataUsingZod.ts
@@ -2,6 +2,9 @@ import type { z } from 'zod'
 import { zodErrorToFormErrors } from '~/utils/zodErrorToFormErrors'
 import ValidationError from '~/error/ValidationError'
 
+/**
+ * @throws ValidationError
+ */
 export function validateDataUsingZod<T>(data: unknown, schema: z.ZodTypeAny<T>, message?: string): T {
     const {
         success,
