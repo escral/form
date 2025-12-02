8faf6d0a2897e5acd057e19f8d6014c14cf46a9e
Alexander Topalo <topaloalexander@gmail.com>
2025-12-03 01:27:44 +0200

    fix type

--- src/validation/useCallbacksFormValidation.ts
+++ src/validation/useCallbacksFormValidation.ts
@@ -1,7 +1,6 @@
-import type Errors from '~/lib/Errors'
 import type { ValidationRulesSet } from '~/types/validation'
 import { validateDataUsingCallbacks } from '~/validation/validateDataUsingCallbacks'
 
-export function useCallbacksFormValidation<T>(validationRulesSet: ValidationRulesSet): (data: unknown, errors: Errors) => T {
+export function useCallbacksFormValidation<T>(validationRulesSet: ValidationRulesSet): (data: unknown) => T {
     return (data: unknown) => validateDataUsingCallbacks(data, validationRulesSet)
 }
